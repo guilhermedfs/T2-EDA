2685d151fa7b7eecc2c5922eca34c63a874f2796
Victor <victorear05@gmail.com>
2021-05-11 13:08:13 -0300

    Re-Criando Reposi´tório

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "BibTrab.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    FILE *file;
+    char nomeArquivo[256];
+    int op = 0;
+    No* PNo = NULL;
+
+    do{
+    op = menu();
+        switch (op){
+        case 1:
+            printf("Digite o nome do arquivo que deseja abrir\n");
+            scanf("%s", nomeArquivo);
+            file = fopen(nomeArquivo, "r");
+            if(file){
+                No* PNo = AbreArquivo(file);
+            }
+            else
+                printf("Erro ao abrir arquivo!\n");
+            break;
+        case 2:
+            printf("Calcula Fator de Balanceamento\n");
+            break;
+        case 3:
+            printf("Imprime ABP\n");
+            break;
+        case 0:
+            fclose(file);
+            printf("\n\n-------------------- Programa Encerrado --------------------\n\n");
+            break;
+        default:
+            printf("Op��o Inv�lida!\n");
+            break;
+        }
+    }while (op != 0);
+    return 0;
+}
